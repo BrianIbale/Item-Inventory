a6c9f7f24b58d5f5da5ff322f2428ea33262a90a
BrianIbale <ibalebrianmathew@gmail.com>
2024-04-05 12:59:59 +0800

    Setup database and create models item and service

--- item_inventory/items/models.py
+++ item_inventory/items/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    item_name = models.CharField(max_length=200)
+    item_quantity = models.IntegerField(default=0)
+    item_price = models.IntegerField(default=0)
+
+
+class Service(models.Model):
+    service_name = models.CharField(max_length=200)
+    service_quantity = models.IntegerField(default=0)
+    service_price = models.IntegerField(default=0)
